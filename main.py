91a2e8f0a14fb00bb9a94a0c5ebe1dfff3eb7400
Lesley20 <jinmei.li@baruchmail.cuny.edu>
2020-02-29 21:35:49 -0500

    modified main.py

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import src.api
 
 if __name__ == "__main__":
 	args = sys.argv[1:]
+	num_args = len(args)
+
+
+	page_size = args[0]
+	num_pages = args[1]
+	output = args[2]
+
+	try:
+		if 
+
+	except Exception as e:
+		print(f'Something went wrong {e}')
+
+		raise 
