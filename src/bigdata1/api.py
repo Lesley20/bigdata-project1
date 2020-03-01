36929cea3ae84a11bebb23e9787ec44a8c245823
Lesley20 <jinmei.li@baruchmail.cuny.edu>
2020-02-29 21:42:28 -0500

    modified on apipy

--- src/bigdata1/api.py
+++ src/bigdata1/api.py
@@ -3,11 +3,17 @@
 import os
 from sodapy import Socrata
 
+def get_data(page_size: float) -> dict:
+	try: 
+		client = Socrata('data.cityofnewyork.us', os.environ['APP_KEY'])
 
-client = Socrata('data.cityofnewyork.us', os.environ['APP_KEY'])
+		client.get('nc67-uf89', limit=10)
 
-client.get('nc67-uf89', limit=10)
+		client.get('nc67-uf89', limit=10, offset=10)
 
-client.get('nc67-uf89', limit=10, offset=10)
+		client.get('nc67-uf89',select='COUNT(*)')
+		
+	except Exception as e:
+		print(f'Something went wrong {e}')
 
-client.get('nc67-uf89',select='COUNT(*)')
+		raise 
