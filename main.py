f4bf8d3f1b0cd6186e194586d6e4f78aa12a5d2f
Lesley20 <jinmei.li@baruchmail.cuny.edu>
2020-03-01 06:59:22 -0500

    modified

--- main.py
+++ main.py
@@ -12,24 +12,7 @@ if __name__ == "__main__":
 
 
 
-
-
 """
-(conflict)
-#args = sys.argv[1:]
-#num_args = len(args)
-#page_size = args[0].split('=')[1]
-#num_pages = args[1].split('=')[1]
-#output = args[2]
-"""
-"""
-through process:
-
-1. get argument from main.py
-	page_size
-	page_num
-2. call function from api.py 
-3. api.py will send the request 
-4. then display output in json
+argparse will figure out how to parse those out of sys.argv. 
 
 """
