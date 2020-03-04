c4ab61ce18cf97adfa38f525fc54539f6929e301
Lesley20 <jinmei.li@baruchmail.cuny.edu>
2020-03-04 10:21:39 -0500

    modified

--- src/bigdata1/api.py
+++ src/bigdata1/api.py
@@ -7,10 +7,17 @@ def get_data(page_size: int) -> dict:
 	try: 
 		client = Socrata('data.cityofnewyork.us', os.environ['APP_KEY'])
 
-		client.get('nc67-uf89', limit={page_size})
-		client.get('nc67-uf89',select='COUNT(*)')
+		client.get('nc67-uf89', limit={page_size}) #
+		#client.get('nc67-uf89',select='COUNT(*)')
 
 	except Exception as e:
 		print(f'Something went wrong {e}')
 
 		raise 
+
+def get_result:
+	try:
+		pass
+
+	except Exception as e:
+		pass
