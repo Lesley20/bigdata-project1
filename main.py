cd5714d7757c8915f38658306a46a0bb7a0562f6
Lesley20 <jinmei.li@baruchmail.cuny.edu>
2020-03-04 14:37:26 -0500

    modified

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#import sys 
+import sys 
 import argparse
 
 from src.bigdata1.api import get_data
 
 if __name__ == "__main__":
 	parse = argparse.ArgumentParser(description="Process parameters")
-	parse.add_argument("--page_size", type = int, default = None)
-	parse.add_argument("--num_pages", type = int, default = None)
+	parse.add_argument("--page_size", type = int)
+	parse.add_argument("--num_pages", type = int)
 
 	args = parser.parse_args()
 	get_data(arg.page_size)
