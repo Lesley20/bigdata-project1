a8053bee83e80243069cac27a3bb43e44c243331
Lesley20 <jinmei.li@baruchmail.cuny.edu>
2020-03-04 20:38:41 -0500

    modified

--- src/bigdata1/api.py
+++ src/bigdata1/api.py
@@ -3,11 +3,11 @@
 import os
 from sodapy import Socrata
 
-def get_data(page_size: int, num_page: int):
+def get_data(page_size: int, num_pages: int):
 	try: 
 		client = Socrata('data.cityofnewyork.us', os.environ['APP_KEY'])
 
-		for i in range (num_page): 
+		for i in range (num_pages): 
 			client.get('nc67-uf89', limit={page_size}, offset={})
 			#client.get('nc67-uf89',select='COUNT(*)')
 
